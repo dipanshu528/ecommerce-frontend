c3e515a518e63e315eac24613b32e13d3f49f207
dipanshu528 <vermadipanshu528@gmail.com>
2023-10-05 20:33:46 +0530

    s

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter , Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+
+import Userdetail from "./components/Userdetail";
+import Order from "./components/Order";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+require("dotenv").config();
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+      <Provider store={store}>
+        <HashRouter>
+          <Navbar />
+          
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            
+            <Route path="/cart" element={<Cart />}></Route>
+            <Route path="/detail" element={<Userdetail />}></Route>
+            <Route path="/order" element={<Order />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+          </Routes>
+
+
+          
+        </HashRouter>
+      </Provider>
+      
+    </>
   );
 }
 
